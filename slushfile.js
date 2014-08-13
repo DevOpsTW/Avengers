d34a2d6dea6ccd9d71335c1f1fdc07fc57cb964b
Chieh-Wen Yang <dragon.d.firedrake@gmail.com>
2014-08-13 18:11:31 +0800

    Initial project

---
+++ slushfile.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var gulp     = require('gulp');
+var install  = require('gulp-install');
+var conflict = require('gulp-conflict');
+var template = require('gulp-template');
+var rename   = require('gulp-rename');
+var inquirer = require('inquirer');
+
+gulp.task()
